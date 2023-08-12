1d624a9186edc9be2945870d38701a225134757f
arvind4403 <kr.arvind027@gmail.com>
2023-08-12 21:39:03 +0530

    This is second day in javascript

---
+++ javascript002.js
@@ -0,0 +1,25 @@
+// function greet(){
+//     console.log("Heelo Arvind, How are you ?");
+// }
+// greet();
+
+// function addnumber(a,b){
+//     let sum= a+b;
+//     console.log("sum:",sum);
+// }
+// addnumber(3,4);
+
+
+// function calculatearea(width, height){
+//     let area = width * height ;
+//     return area;
+// }
+// let rectangleArea = calculatearea(4,3);
+// console.log("Rectangle Area:", rectangleArea);
+
+
+function CelsiustoFahrenheit(c){
+    let  f = (c * 9/5) + 32;
+    return f;
+}
+console.log(CelsiustoFahrenheit(77));
