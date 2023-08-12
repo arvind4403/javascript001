2ee06e58a6b053c37a23cfd990655a64dc890617
arvind4403 <kr.arvind027@gmail.com>
2023-08-12 20:56:25 +0530

    javascript 001

---
+++ learning.js
@@ -0,0 +1,49 @@
+//Declear veriable 
+
+// var age =25;
+// let name = "Arvind";
+// const PI = true;
+
+//Experiment with different data types
+
+// var num = 10;
+// var massage = "Hello, this is arvind kumar";
+// var isActive = true;
+
+// var sum =10+5;
+// var full_name="Arvind" + " " + "Kumar"
+// console.log(full_name);
+
+
+// let age = 18;
+// if(age == 20){
+//     console.log("You Are eligible for drink");
+// }else {
+//     console.log("You are not eligible for drink");
+// }
+
+// let myage = 18;
+// let age = "18";
+// if(age === myage){
+//     console.log("You Are eligible for drink");
+// }else {
+//     console.log("You are not eligible for drink");
+// }
+
+
+// for( i=0; i<=9; i++){
+//     console.log("count:",i);
+// }
+
+let countdown=9;
+while(countdown > 0){
+    console.log("countdown:",countdown);
+    countdown--;
+}
+
+
+
+
+
+
+
